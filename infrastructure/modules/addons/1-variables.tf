49a866f7d2192ec0f5c194216a75a9c2f05f7f6c
Luka Krapic <luka.krapic@gmail.com>
2024-10-16 01:58:21 +0200

    feat: add ebs csi driver instalation feat: add efs csi driver instalation ( part setup )
    Still need to refactor efs csi driver:
    - move storage to a separate module
    - mode storage class to a separate module
    - mode efs controller instalation to a separate module
    - cleanup terragrunt files

--- infrastructure/modules/addons/1-variables.tf
+++ infrastructure/modules/addons/1-variables.tf
@@ -16,8 +16,27 @@ variable "pod_identity_version" {
   default     = "v1.3.2-eksbuild.2"
 }
 
+
+variable "ebs_csi_driver_version" {
+  type        = string
+  description = "Version of the ebs csi driver."
+  default     = "v1.35.0-eksbuild.1"
+}
+
 variable "cluster_autoscaler_chart_version" {
   type        = string
   description = "Helm chart version of cluster autoscaler"
   default     = "9.37.0"
 }
+
+variable "efs_storage_configuration" {
+  description = "Efs storage configuration"
+  type = object({
+    subnet_ids                          = list(string)
+    security_group_ids                  = list(string)
+    storage_class_name                  = string
+    storage_class_directory_permissions = string
+  })
+}
+
+
