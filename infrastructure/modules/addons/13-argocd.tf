305c243f89474db639cd4e643dd732dd7c303a21
Luka Krapic <luka.krapic@gmail.com>
2024-11-02 18:23:38 +0100

    feat: template argocd configuration

--- infrastructure/modules/addons/13-argocd.tf
+++ infrastructure/modules/addons/13-argocd.tf
@@ -7,7 +7,13 @@ resource "helm_release" "argocd" {
 
   namespace        = "argocd"
   create_namespace = true
-  values           = [file("values/argocd.yaml")]
+  values = [
+    templatefile("values/argocd.yaml", {
+      hosted_zone_name    = var.hosted_zone_name
+      ingress_class_name  = "external-nginx"
+      cluster_issuer_name = "dns-01-cluster-issuer"
+    })
+  ]
 
   depends_on = [helm_release.cert-manager, helm_release.external_dns, helm_release.external_nginx]
 }
